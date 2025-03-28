7c70bba81306fc4bb4d593766b7ab237518e2c7f
Xavier Akers <x-akers@hotmail.com>
2025-03-28 13:13:52 -0400

    dummy data

---
+++ database-schema/dummy_data/dummy_venues.sql
@@ -0,0 +1,21 @@
+INSERT INTO venues (name, location) VALUES
+('Cassell Coliseum', 'Blacksburg, VA'),
+('Lane Stadium', 'Blacksburg, VA'),
+('Hokie Stadium', 'Blacksburg, VA'),
+('Moss Arts Center', 'Blacksburg, VA'),
+('Burrows Hall', 'Blacksburg, VA'),
+('Torgersen Hall', 'Blacksburg, VA'),
+('The Inn at Virginia Tech', 'Blacksburg, VA'),
+('Squires Student Center', 'Blacksburg, VA'),
+('Fralin Life Science Institute', 'Blacksburg, VA'),
+('Dewberry Hall', 'Blacksburg, VA'),
+('Vawter Hall', 'Blacksburg, VA'),
+('University Mall', 'Blacksburg, VA'),
+('Newman Library', 'Blacksburg, VA'),
+('VT Graduate Life Center', 'Blacksburg, VA'),
+('Smithfield Plantation', 'Blacksburg, VA'),
+('Alumni Hall', 'Blacksburg, VA'),
+('Brooks Hall', 'Blacksburg, VA'),
+('Goodwin Hall', 'Blacksburg, VA'),
+('Liberal Arts Building', 'Blacksburg, VA');
+
